42718b5e14412fccc7e790ee4e7e6ee3355fac4c
Nhựt Tân <119086524+Nguyennhuttan2004@users.noreply.github.com>
2024-07-30 19:40:26 +0700

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux" ;
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/configStore.js";
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store ={store}>
- <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+import { Provider } from "react-redux";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
- 
-)
-  
+);
